88f3d1d5c5aa6efe277575fe60d1accff9bab61b
a.chepugov <oleksii.chepugov@ria.com>
2018-08-14 18:28:11 +0300

    chore

--- source/index.js
+++ source/index.js
@@ -1,16 +1,17 @@
 'use strict';
+
 const {
 	Standard: {
 		Promise: {sleep, promisify},
-		Number: {random},
+		Number: {random}
 	}
-} = require("helpers-js");
+} = require('helpers-js');
 
 /**
- * Создает кеширующую обертку для функции
- * @name cachify
+ * Wrap function with caching layer
+ * @name cachify-wrapper
  * @param {Object} fn
- * @param {Object} cache - CacheInterface (must provide set, get & expire methods)
+ * @param {Object} cache - CacheInterface for KVStorage (must provide set, get & expire methods)
  * @param {Object} [options]
  * @param {Number|Object} [options.expire] - key expire options
  * @param {Number} [options.expire.ttl=Infinity] - cached data ttl
@@ -19,10 +20,30 @@ const {
  * @param {Number} [options.lock.timeout=1000] - lock timeout
  * @param {String} [options.lock.placeholder='?'] - lock placeholder
  * @param {Number} [options.latency=options.lock.timeout] - time to awaiting responce from source
- * @param {Number} [options.retries=3] - multiplier for `options.latency` to awaiting responce from source before new call
+ * @param {Number} [options.retries=3] - multiplier for `options.latency` to awaiting response from source before new call
+ * @param {String} [options.prefix = ''] - prefix for `cache` KVStorage keys
  * @param {Boolean|Object} [options.stale] - allow to use a stale data
  * @param {Number} [options.stale.lock=options.retries*options.latency] - lock timout for updating stale data
  * @param {Function} [options.hasher=JSON.stringify] - function for creating key for KV-storage
+ * @return {Function} wrapped function
+ * @example
+ * const wrapper = require('cachify-wrapper');
+ * const redis = require('redis');
+ * class RedisCache {
+ *  constructor() {
+ *   const client = redis.createClient();
+ *   this.set = (key, value) => new Promise((resolve, reject) => client.set(key, JSON.stringify(value), (error, value) => error ? reject(error) : resolve(value)));
+ *   this.get = (key) => new Promise((resolve, reject) => client.get(key, (error, value) => error ? reject(error) : resolve(JSON.parse(value))));
+ *   this.expire = (key, expire) => new Promise((resolve, reject) => client.pexpire(key, expire, (error) => error ? reject(error) : resolve()));
+ *  }
+ * }
+ * const cache = new RedisCache();
+ * const sourceFunc = (a) => new Promise((resolve, reject) => setTimeout(() => resolve({data: a}), 2000));
+ * const options = {expire: {ttl: 5000}, lock: {timeout: 5000, placeholder: '???'}, latency: 1000, retries: 3, prefix: '__sourceFunc__:'};
+ * const cachified = wrapper(sourceFunc, cache, options);
+ * cachified('123').then((payload) => console.dir(payload, {colors: true, depth: null}));
+ * setTimeout(() => cachified('123').then((payload) => console.dir(payload, {colors: true, depth: null})), 1500);
+ * setTimeout(() => cachified('123').then((payload) => console.dir(payload, {colors: true, depth: null})), 2100);
  */
 module.exports = function (fn, cache, {expire: {ttl, deviation} = {}, expire, lock: {timeout, placeholder = '?'} = {}, stale, hasher = JSON.stringify, latency, retries, prefix = ''} = {}) {
 	ttl = Number.isFinite(ttl) ?
@@ -88,12 +109,12 @@ module.exports = function (fn, cache, {expire: {ttl, deviation} = {}, expire, lo
 
 				if (payload) {
 					if (
-						stale
-						&& (Date.now() > response.timestamp + ttl)
-						&& (!response.stale || (Date.now() > response.stale + staleLock))
+						stale &&
+						(Date.now() > response.timestamp + ttl) &&
+						(!response.stale || (Date.now() > response.stale + staleLock))
 					) {
 						Promise.all([promisifiedFN.apply(undefined, arguments), cacheLockStale(key, response).catch(console.error)])
-							.then(([responce]) => handleResponse(key, responce))
+							.then(([responce]) => handleResponse(key, responce));
 					}
 					return payload;
 				} else {
@@ -102,6 +123,11 @@ module.exports = function (fn, cache, {expire: {ttl, deviation} = {}, expire, lo
 						Promise.all([promisifiedFN.apply(undefined, arguments), cacheLock(key).catch(console.error)])
 							.then(([responce]) => handleResponse(key, responce));
 				}
-			})
-	}
+			});
+	};
+
+	/**
+	 * npm install --save cachify-wrapper
+	 * @name Installation
+	 */
 };
