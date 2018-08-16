d1151f7ece93435aa14793a2ecc3eb69ab0d75dd
a.chepugov <oleksii.chepugov@ria.com>
2018-08-16 14:51:20 +0300

    2.0.1

--- source/index.js
+++ source/index.js
@@ -11,7 +11,7 @@ const {
  * Wrap function with caching layer
  * @name cachify-wrapper
  * @param {Object} fn
- * @param {Object} cache - CacheInterface for KVStorage (must provide set, get & expire methods)
+ * @param {Object} cache - KVStorage [must provide set(key, value, expired), get(key) methods]
  * @param {Object} [options]
  * @param {Number|Object} [options.expire] - key expire options
  * @param {Number} [options.expire.ttl=Infinity] - cached data ttl
@@ -19,12 +19,12 @@ const {
  * @param {Object} [options.lock] - lock similar initial request to data source
  * @param {Number} [options.lock.timeout=1000] - lock timeout
  * @param {String} [options.lock.placeholder='?'] - lock placeholder
- * @param {Number} [options.latency=options.lock.timeout] - time to awaiting responce from source
- * @param {Number} [options.retries=3] - multiplier for `options.latency` to awaiting response from source before new call
- * @param {String} [options.prefix = ''] - prefix for `cache` KVStorage keys
+ * @param {Number} [options.latency=options.lock.timeout] - expected source response time (with `options.retries` affect on awaiting for duplicate requests for first request result)
+ * @param {Number} [options.retries=(options.lock.timeout/options.latency)+1] - number of passes before new actual request
+ * @param {String} [options.prefix=''] - prefix for keys in KVStorage
  * @param {Boolean|Object} [options.stale] - allow to use a stale data
- * @param {Number} [options.stale.lock=options.retries*options.latency] - lock timout for updating stale data
- * @param {Function} [options.hasher=JSON.stringify] - function for creating key for KV-storage
+ * @param {Number} [options.stale.lock=options.lock.timeout] - lock timeout for updating stale data
+ * @param {Function} [options.hasher=JSON.stringify] - creating key for KV-storage function
  * @return {Function} wrapped function
  * @example
  * const wrapper = require('cachify-wrapper');
@@ -32,20 +32,33 @@ const {
  * class RedisCache {
  *  constructor() {
  *   const client = redis.createClient();
- *   this.set = (key, value) => new Promise((resolve, reject) => client.set(key, JSON.stringify(value), (error, value) => error ? reject(error) : resolve(value)));
- *   this.get = (key) => new Promise((resolve, reject) => client.get(key, (error, value) => error ? reject(error) : resolve(JSON.parse(value))));
- *   this.expire = (key, expire) => new Promise((resolve, reject) => client.pexpire(key, expire, (error) => error ? reject(error) : resolve()));
+ *   this.set = (key, value, expire) =>
+ *    new Promise((resolve, reject) =>
+ *     client.set(key, JSON.stringify(value),
+ *      (error, value) =>
+ *       error ?
+ *        reject(error) :
+ *         expire ?
+ *          client.pexpire(key, expire, (error) => error ? reject(error) : resolve(value)) :
+ *          resolve(value)));
+ *
+ *   this.get = (key) =>
+ *    new Promise((resolve, reject) =>
+ *     client.get(key, (error, value) =>
+ *      error ?
+ *       reject(error) :
+ *       resolve(JSON.parse(value))));
  *  }
  * }
  * const cache = new RedisCache();
- * const sourceFunc = (a) => new Promise((resolve, reject) => setTimeout(() => resolve({data: a}), 2000));
- * const options = {expire: {ttl: 5000}, lock: {timeout: 5000, placeholder: '???'}, latency: 1000, retries: 3, prefix: '__sourceFunc__:'};
- * const cachified = wrapper(sourceFunc, cache, options);
- * cachified('123').then((payload) => console.dir(payload, {colors: true, depth: null}));
- * setTimeout(() => cachified('123').then((payload) => console.dir(payload, {colors: true, depth: null})), 1500);
- * setTimeout(() => cachified('123').then((payload) => console.dir(payload, {colors: true, depth: null})), 2100);
+ * const sourceFunc = (a) => new Promise((resolve) => setTimeout(() => resolve(a * 2), 250));
+ * const options = {expire: {ttl: 500}, lock: {timeout: 100, placeholder: '???'}, latency: 100, retries: 1, prefix: '__sourceFunc__:'};
+ * const cached = wrapper(sourceFunc, cache, options);
+ * cached(123).then((payload) => console.dir(payload, {colors: true, depth: null})); // Invoke new request
+ * setTimeout(() => cached(123).then((payload) => console.dir(payload, {colors: true, depth: null})), 200); // Will get cached result
+ * setTimeout(() => cached(123).then((payload) => console.dir(payload, {colors: true, depth: null})), 50); // Will invoke new actual request (because of low retries & latency options it can't wait for first invoke cache)
  */
-module.exports = function (fn, cache, {expire: {ttl, deviation} = {}, expire, lock: {timeout, placeholder = '?'} = {}, stale, hasher = JSON.stringify, latency, retries, prefix = ''} = {}) {
+module.exports = function (fn, cache, {expire: {ttl, deviation} = {}, expire, lock: {timeout, placeholder = '?'} = {}, stale, hasher, latency, retries, prefix = ''} = {}) {
 	ttl = Number.isFinite(ttl) ?
 		ttl > 0 ?
 			ttl :
@@ -59,9 +72,9 @@ module.exports = function (fn, cache, {expire: {ttl, deviation} = {}, expire, lo
 	timeout = Number.isFinite(timeout) && timeout > 0 ? timeout : 1000;
 
 	latency = Number.isFinite(latency) && latency > 0 ? latency : timeout;
-	retries = Number.isFinite(retries) && retries > 0 ? retries : 3;
+	retries = Number.isFinite(retries) && retries > 0 ? retries : Math.floor(timeout / latency) + 1;
 
-	const staleLock = stale && stale.lock && Number.isFinite(stale.lock) && stale.lock > 0 ? stale.lock : latency * retries;
+	const staleLock = stale && stale.lock && Number.isFinite(stale.lock) && stale.lock > 0 ? stale.lock : timeout;
 
 	prefix = typeof prefix === 'string' ? prefix : '';
 
@@ -69,11 +82,10 @@ module.exports = function (fn, cache, {expire: {ttl, deviation} = {}, expire, lo
 
 	const cacheSet = promisify(cache.set, cache);
 	const cacheGet = promisify(cache.get, cache);
-	const cacheExpire = promisify(cache.expire, cache);
 
-	const cacheLock = (key) =>
-		cacheSet(key, placeholder)
-			.then(() => timeout !== Infinity ? cacheExpire(key, timeout) : Promise.resolve());
+	const cacheLock = timeout === Infinity ?
+		(key) => cacheSet(key, placeholder) :
+		(key) => cacheSet(key, placeholder, timeout);
 
 	const cacheLockStale = (key, response = {}) => cacheSet(key, Object.assign({}, response, {stale: Date.now()}));
 
@@ -95,13 +107,16 @@ module.exports = function (fn, cache, {expire: {ttl, deviation} = {}, expire, lo
 			return payload;
 		} :
 		(key, payload) => {
-			cacheSet(key, {payload, timestamp: Date.now()})
-				.then(() => cacheExpire(key, ttl + random(0, deviation)));
+			cacheSet(key, {payload, timestamp: Date.now()}, Math.floor(ttl + random(0, deviation)));
 			return payload;
 		};
 
+	hasher = hasher ?
+		hasher :
+		(a, prefix) => prefix + JSON.stringify(a);
+
 	return function () {
-		const key = prefix + hasher(arguments);
+		const key = hasher(arguments, prefix);
 
 		return cacheGet(key).catch()
 			.then((response) => {
@@ -119,10 +134,13 @@ module.exports = function (fn, cache, {expire: {ttl, deviation} = {}, expire, lo
 					return payload;
 				} else {
 					return response === placeholder ?
-						getOnLock(key) :
+						getOnLock(key).catch(() =>
+							Promise.all([promisifiedFN.apply(undefined, arguments), cacheLock(key).catch(console.error)])
+								.then(([response]) => handleResponse(key, response))) :
 						Promise.all([promisifiedFN.apply(undefined, arguments), cacheLock(key).catch(console.error)])
-							.then(([responce]) => handleResponse(key, responce));
+							.then(([response]) => handleResponse(key, response));
 				}
+
 			});
 	};
 
